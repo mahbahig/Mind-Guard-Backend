f4044d935964a9585add5dc84eb2adc47dd000c8
mahbahig <mahbahig2@gmail.com>
2025-12-09 00:49:33 +0200

    Added role based guards

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,17 @@
+import { Roles } from '@common/decorators';
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const roles = this.reflector.getAllAndMerge(Roles, [context.getClass(), context.getHandler()]);
+
+    if (!request.user) throw new UnauthorizedException('Please login to access this resource');
+    if (!roles.includes(request.user.role)) throw new UnauthorizedException('Access denied. You are not authorized to access this resource');
+
+    return true;
+  }
+}
