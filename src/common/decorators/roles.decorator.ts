49bdeeaac7b7a0594af3e163b555280ac82793de
mahbahig <mahbahig2@gmail.com>
2025-12-09 00:25:59 +0200

    Edited roles decorator to accept only array of UserRole enum

--- src/common/decorators/roles.decorator.ts
+++ src/common/decorators/roles.decorator.ts
@@ -1,3 +1,4 @@
 import { Reflector } from '@nestjs/core';
+import { UserRole } from '@shared/enums';
 
-export const Roles = Reflector.createDecorator<string[]>();
+export const Roles = Reflector.createDecorator<UserRole[]>();
